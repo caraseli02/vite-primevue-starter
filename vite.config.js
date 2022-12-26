2e104ee44c1375f5b2f26c51af1b290889bad195
vlad <caraseli02@gmail.com>
2022-12-26 16:46:39 +0100

    clear components and tests

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    include: ['test/**/*.test.ts'],
+    include: ['src/components/**/*.test.ts'],
     environment: 'happy-dom',
   },
 

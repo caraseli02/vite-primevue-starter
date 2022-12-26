3842687dbd4d1ac0d5982a4368867b371d1007b2
caraseli02 <caraseli02@gmail.com>
2022-12-26 23:48:37 +0100

    fix: component logic and test case

--- src/components/Atoms/checkbox/Checkbox.test.ts
+++ src/components/Atoms/checkbox/Checkbox.test.ts
@@ -1,66 +1,59 @@
-import { mount } from '@vue/test-utils'
-import { describe, expect, test, vi } from 'vitest'
-import Checkbox from './Checkbox.vue'
+import { mount } from '@vue/test-utils';
+import { describe, expect, test, vi } from 'vitest';
+import Checkbox from './Checkbox.vue';
 
 describe('Checkbox.vue', () => {
   test('should render the checkbox element', () => {
-    const wrapper = mount(Checkbox)
-    expect(wrapper.find('.p-checkbox.p-component').exists()).toBe(true)
-  })
+    const wrapper = mount(Checkbox);
+    expect(wrapper.find('.p-checkbox.p-component').exists()).toBe(true);
+  });
 
   test('should not apply the p-checkbox-checked class when modelValue prop is false', () => {
     const wrapper = mount(Checkbox, {
       props: {
+        binary: true,
         modelValue: false,
       },
-    })
-    expect(wrapper.classes()).not.toContain('p-checkbox-checked')
-  })
+    });
+    expect(wrapper.classes()).not.toContain('p-checkbox-checked');
+  });
 
   test('should apply the p-checkbox-checked class when modelValue prop is true', () => {
     const wrapper = mount(Checkbox, {
       props: {
+        binary: true,
         modelValue: true,
       },
-    })
-    expect(wrapper.classes()).toContain('p-checkbox-checked')
-  })
+    });
+    expect(wrapper.classes()).toContain('p-checkbox-checked');
+  });
 
   test('should apply the p-checkbox-disabled class when disabled prop is true', () => {
     const wrapper = mount(Checkbox, {
       props: {
         disabled: true,
       },
-    })
-    expect(wrapper.classes()).toContain('p-checkbox-disabled')
-  })
+    });
+    expect(wrapper.classes()).toContain('p-checkbox-disabled');
+  });
 
   test('should apply the p-checkbox-focused class when input element is focused', async () => {
-    const wrapper = mount(Checkbox)
-    await wrapper.find('input').trigger('focus')
-    expect(wrapper.classes()).toContain('p-checkbox-focused')
-  })
+    const wrapper = mount(Checkbox);
+    await wrapper.find('input').trigger('focus');
+    expect(wrapper.classes()).toContain('p-checkbox-focused');
+  });
 
   test('should not apply the p-checkbox-focused class when input element is blurred', async () => {
-    const wrapper = mount(Checkbox)
-    await wrapper.find('input').trigger('focus')
-    await wrapper.find('input').trigger('blur')
-    expect(wrapper.classes()).not.toContain('p-checkbox-focused')
-  })
+    const wrapper = mount(Checkbox);
+    await wrapper.find('input').trigger('focus');
+    await wrapper.find('input').trigger('blur');
+    expect(wrapper.classes()).not.toContain('p-checkbox-focused');
+  });
 
-  test('should emit a "click" event when the checkbox is clicked', async () => {
-    const wrapper = mount(Checkbox)
-    const clickHandler = vi.fn()
-    wrapper.vm.$on('click', clickHandler)
-    await wrapper.find('.p-checkbox.p-component').trigger('click')
-    expect(clickHandler).toHaveBeenCalled()
-  })
-
-  test('should emit a "update:modelValue" event when the checkbox is clicked', async () => {
-    const wrapper = mount(Checkbox)
-    const updateHandler = vi.fn()
-    wrapper.vm.$on('update:modelValue', updateHandler)
-    await wrapper.find('.p-checkbox.p-component').trigger('click')
-    expect(updateHandler).toHaveBeenCalled()
-  })
-})
+  test('should emit a "click" & "update:modelValue" events when the checkbox is clicked', async () => {
+    const wrapper = mount(Checkbox);
+    await wrapper.find('.p-checkbox.p-component').trigger('click');
+    expect(wrapper.emitted()).toHaveProperty('click');
+    expect(wrapper.emitted()).toHaveProperty('update:modelValue');
+  });
+});
